c87154c11dd2be3a43cf74bc0f8675d15f585426
denden2004 <mary.ylagan@lpunetwork.edu.ph>
2025-04-12 10:27:48 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAeBLawmzg48DnhE3ZBvNe1W67GJqKEC-U",
+  authDomain: "ylagan-midterms-act2.firebaseapp.com",
+  databaseURL: "https://ylagan-midterms-act2-default-rtdb.firebaseio.com",
+  projectId: "ylagan-midterms-act2",
+  storageBucket: "ylagan-midterms-act2.firebasestorage.app",
+  messagingSenderId: "132931544067",
+  appId: "1:132931544067:web:b936c8ca4507e6cc11da36"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
